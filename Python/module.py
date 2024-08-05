1ca11480c8532b15f0d5cbe3d92c906e8d2add81
ahmed_salem <100295040+ahmedsallem@users.noreply.github.com>
2024-08-05 15:43:28 +0300

    Add files via upload

---
+++ Python/module.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def add(n1,n2):
+    return n1+n2
+
+
+# In[2]:
+
+
+def multip(n1,n2):
+    return n1 * n2
+
+
+# In[ ]:
+
+
+
+
---
+++ Python/module_1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def add(n1,n2,n3):
+    return n1+n2+n3
+
+
+# In[2]:
+
+
+def multp(n1,n2,n3):
+    return n1 * n2 *n3
+
+
+# In[ ]:
+
+
+
+
